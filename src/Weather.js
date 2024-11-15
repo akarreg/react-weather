dcc2202a0276d9c09d0d3456bd12a54bce504109
akarreg <161454874+akarreg@users.noreply.github.com>
2024-11-15 12:28:16 +0100

    axios add

--- src/Weather.js
+++ src/Weather.js
@@ -1,32 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form className="row">
-        <input className="col-8" type="search " placeholder="Enter city....." />
-        <input className="col-3 Search" type="Submit" />
-      </form>
+  const [WeatherData, setWeatherData] = useState(null);
+  const [ready, setready] = useState(false);
 
-      <main>
-        <div>
-          <h1 className="City">City name</h1>
-          <h3>description</h3>
-          <div className="row">
-            <div className="col">
-              emoji
-              <span className="Temperature"> 6</span>
-              <span className="degrees">˚C</span>
-            </div>
+  function handleResponse(response) {
+    console.log(response.main);
 
-            <div className="col">
-              <div className="">Wind:</div>
-              <div className="">visibility:</div>
-              <div className="">Humidity:</div>
+    setWeatherData(response.data.main.temp);
+    setready(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form className="row">
+          <input
+            className="col-9 Search"
+            type="search "
+            placeholder="Enter city....."
+          />
+          <input className="col-3 Submit" type="Submit" />
+        </form>
+
+        <main>
+          <div>
+            <h1 className="City">City name</h1>
+            <h3>description</h3>
+            <div className="row">
+              <div className="col">
+                emoji
+                <span className="Temperature"> {Math.round(WeatherData)}</span>
+                <span className="degrees">˚C</span>
+              </div>
+
+              <div className="col">
+                <div className="">Wind:</div>
+                <div className="">visibility:</div>
+                <div className="">Humidity:</div>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-    </div>
-  );
+        </main>
+      </div>
+    );
+  } else {
+    const apiKey = "7059cb165caa3316bff682d263a01b1e";
+    let city = "ondarroa";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading....";
+  }
 }
