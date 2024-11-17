f2d368813523663040bf198eaeb2036ce9bd00fd
akarreg <161454874+akarreg@users.noreply.github.com>
2024-11-17 07:58:34 +0100

    add a github

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <Weather defaultcity="doha" />
 
       <footer>
-        This page was create and run by Ama Akarregi is hots in githun and
-        Netflix
+        This page was create and run by Ama Akarregi is hots in{" "}
+        <a href="https://github.com/akarreg/weather-react">Github</a>and Netflix
       </footer>
     </div>
   );
